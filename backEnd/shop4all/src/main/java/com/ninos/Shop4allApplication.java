b42b61493319db19d69e65c536cfe7cd47d54a07
NinoosMoshi <nalkaton@gmail.com>
2025-08-01 12:05:20 -0500

    init project

---
+++ backEnd/shop4all/src/main/java/com/ninos/Shop4allApplication.java
@@ -0,0 +1,13 @@
+package com.ninos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Shop4allApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Shop4allApplication.class, args);
+	}
+
+}
